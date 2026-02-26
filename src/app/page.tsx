398c823563e51a601f611cd74c344b802f804be4
gaby-calypso <cfgaby.calipso@gmail.com>
2026-02-25 23:18:23 -0600

    feat: add search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { Navbar } from "@/components/Navbar/Navbar";
 import { Hero } from "@/components/Hero/Hero";
 import { CocktailCard } from "@/components/CocktailCard/CocktailCard";
+import { SearchBar } from "@/components/SearchBar/SearchBar";
 import { searchCocktails, Cocktail } from "@/services/cocktails";
 
 export default function Home() {
@@ -15,10 +16,19 @@ export default function Home() {
     searchCocktails("a").then(setCocktails);
   }, []);
 
+  const handleSearch = (query: string) => {
+    if (query.length > 2) {
+      searchCocktails(query).then(setCocktails);
+    } else if (query.length === 0) {
+      searchCocktails("a").then(setCocktails);
+    }
+  };
+
   return (
     <main>
       <Navbar />
       <Hero />
+      <SearchBar onSearch={handleSearch} />
       <div className="p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {cocktails.map((cocktail) => (
           <CocktailCard
