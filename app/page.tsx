e4bcce99d8f95ee1eaf06757630dd6577b3171dd
gaby-calypso <cfgaby.calipso@gmail.com>
2026-02-25 22:19:06 -0600

    feat: add CocktailCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,32 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar/Navbar";
 import { Hero } from "@/components/Hero/Hero";
 import { CocktailCard } from "@/components/CocktailCard/CocktailCard";
+import { searchCocktails, Cocktail } from "@/services/cocktails";
 
 export default function Home() {
+  const [cocktails, setCocktails] = useState<Cocktail[]>([]);
+
+  useEffect(() => {
+    searchCocktails("a").then(setCocktails);
+  }, []);
+
   return (
     <main>
       <Navbar />
       <Hero />
-      <div className="p-8 grid grid-cols-3 gap-6">
-        <CocktailCard
-          imageUrl="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
-          cocktailName="Margarita"
-          category="Clásico"
-          onClick={() => alert("clicked!")}
-        />
-        <CocktailCard
-          imageUrl="https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg"
-          cocktailName="Mojito"
-          category="Tropical"
-          onClick={() => alert("clicked!")}
-        />
-        <CocktailCard
-          imageUrl="https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg"
-          cocktailName="Old Fashioned"
-          category="Clásico"
-          onClick={() => alert("clicked!")}
-        />
+      <div className="p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {cocktails.map((cocktail) => (
+          <CocktailCard
+            key={cocktail.idDrink}
+            imageUrl={cocktail.strDrinkThumb}
+            cocktailName={cocktail.strDrink}
+            category={cocktail.strCategory}
+            onClick={() => console.log(cocktail.idDrink)}
+          />
+        ))}
       </div>
     </main>
   );
